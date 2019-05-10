537d5233c6a755659e6c50848e206b49115bf095
aboriginal <cyw350446550@163.com>
2019-05-10 23:13:46 +0800

    feat:add the algorithm of lstm

--- alogirithm_model/lstm/realize.py
+++ alogirithm_model/lstm/realize.py
@@ -17,99 +17,161 @@ num_units = 100  # 隐藏层神经元个数
 iterations = 250  # 迭代次数
 batch_size = 50  # 每次传入50个
 
+all_register = "./../../data/all_register.csv"
+# src_path = './../../data/individual_register/1234567-2019-5-9-21.csv'
+test_src_path = './../../data/individual_login/7654321-2019-5-10-22.csv'
+
 
 # 读取csv文件
-def read_csv():
-    dataframe = pd.read_csv("./../../data/pre_collected_data.csv", header=None)
-    x = dataframe.iloc[:, :450]  # 第0-449列
-    y = dataframe.iloc[:, 450]  # 第450列
-    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, test_size=0.2)
+def read_csv(src_path, username):
+    data1 = np.loadtxt(src_path, dtype=float, delimiter=',').reshape(-1, 451)  # 4 是指第 5 列
+    print(data1)
+    print(data1.shape)
+    x_train, y_train = np.split(data1, (450,), axis=1)  # axis=0 为横向切分 ， axis=1 为纵向切分
+    for i in range(y_train.shape[0]):
+        if str(int(y_train[i][0])) != username:
+            y_train[i][0] = 0  # 不能用这个做注册
+        else:
+            y_train[i][0] = 1  # 不能用这个做注册
+    # x_test, y_test = np.split(data2, (450,), axis=1)  # axis=0 为横向切分 ， axis=1 为纵向切分
+    # print(y_train)
+    y_train = y_train.reshape(1, -1)[0]
+    # print(y_train)
     y_train = y_train.astype(np.int)
-    y_test = y_test.astype(np.int)
+    # y_test = y_test.astype(np.int)
     # 对 标签 独热编码 将存在数据类别的那一类用 1 表示，不存在用 0 表示
     y_train = tf.one_hot(y_train, N_classes)
-    y_test = tf.one_hot(y_test, N_classes)
+    # y_test = tf.one_hot(y_test, N_classes)
 
-    return (x_train, x_test, y_train, y_test)
+    return (x_train, y_train)
 
 
-x_train, x_test, y_train, y_test = read_csv()
 
-# 将测试集和训练集全部转化为numpy类型数据
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    # print(type(y_train))
-    # Tensor 转换成 numpy
-    y_train = y_train.eval(session=sess)
-    # print(type(y_train))
-    y_test = y_test.eval(session=sess)
+def train_handler(src_path, username):
+    x_train, y_train = read_csv(src_path, username)
+    cnt = 0
+    # 将测试集和训练集全部转化为numpy类型数据
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        y_train = y_train.eval(session=sess)
+        x_train = np.array(x_train)
+        # x_test = np.array(x_test)
+    for i in range(y_train.shape[0]):
+        # print(y_train[i][0])
+        # print(username)
+        if int(y_train[i][0]) == 0:
+            cnt +=1
+    print(cnt)
+    if cnt != 10:
+        return '1'
 
-    x_train = np.array(x_train)
-    x_test = np.array(x_test)
+    graph1 = tf.Graph()
+    # print(y_train)
+    with graph1.as_default():
+        x = tf.placeholder(tf.float64, [None, time_step * input_size])
+        image = tf.reshape(x, [-1, time_step, input_size])
+        y = tf.placeholder(tf.float64, [None, N_classes])
+        rnn_cell = tf.nn.rnn_cell.LSTMCell(num_units=num_units)
+        outputs, final_state = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=image, dtype=tf.float64)
 
-graph1 = tf.Graph()
+        output = tf.layers.dense(inputs=outputs[:, -1, :], units=N_classes)
 
-with graph1.as_default():
-    x = tf.placeholder(tf.float64, [None, time_step * input_size])
-    # -1 指根据 x 来确定第一维的数据
-    image = tf.reshape(x, [-1, time_step, input_size])
-    y = tf.placeholder(tf.float64, [None, N_classes])
-    # 建立神经网络（3*100*2）
-    rnn_cell = tf.nn.rnn_cell.LSTMCell(num_units=num_units)
-    outputs, final_state = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=image, dtype=tf.float64)
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=output)
+        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 
-    # print(outputs) # ? * 150 * 100
-    # print(outputs[:, -1, :])
-    # 取最后一个时间戳的 outputs 并送入全连接层 得到 shape=(?, 2) 的 output
-    output = tf.layers.dense(inputs=outputs[:, -1, :], units=N_classes)
-    # print(output)
+        # 取出概率最高的值 并判断 y 跟 output 是否是同一下标
+        # tf.argmax 返回一维向量中最大值的索引
+        # tf.equal 返回一个布尔型的列表
+        correct_prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(output, axis=1))
+        # print(correct_prediction) # shape=(?,1)
 
-    # 用 softmax 求损失值
-    loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=output)
-    # 用 Loss 反向传播，AdamOptimizer 梯度下降
-    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+        # tf.cast 把布尔型列表转换为 float32 ，如[T,T,F]，那么准确率为 66.6%
+        # tf.reduce_mean 对一维向量取平均值
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
 
-    # 取出概率最高的值 并判断 y 跟 output 是否是同一下标
-    # tf.argmax 返回一维向量中最大值的索引
-    # tf.equal 返回一个布尔型的列表
-    correct_prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(output, axis=1))
-    # print(correct_prediction) # shape=(?,1)
+        saver = tf.train.Saver()
+        # print(saver)
 
-    # tf.cast 把布尔型列表转换为 float32 ，如[T,T,F]，那么准确率为 66.6%
-    # tf.reduce_mean 对一维向量取平均值
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+    with tf.Session(graph=graph1) as sess:
+        sess.run(tf.global_variables_initializer())
+        for step in range(iterations):
+            _, loss_ = sess.run([train_op, loss], {x: x_train, y: y_train})
+        saver.save(sess, 'net/my_net.ckpt')
 
-    saver = tf.train.Saver()
-    print(saver)
+    return '1'
 
-with tf.Session(graph=graph1) as sess:
-    sess.run(tf.global_variables_initializer())
-    for step in range(iterations):
-        _, loss_ = sess.run([train_op, loss], {x: x_train, y: y_train})
-        if step % 10 == 0:
-            predict = sess.run(output, {x: x_test[0].reshape(1, -1)})
 
-            accuracy_ = sess.run(accuracy, {x: x_test, y: y_test})
-            print("train loss:", loss_, " accuracy:", accuracy_, "pred", predict)
-    saver.save(sess, 'net/my_net.ckpt')
+def test_handler(src_path, username):
+    x_test, y_test = read_csv(src_path, username)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        y_test = y_test.eval(session = sess)
+    graph1 = tf.Graph()
 
-with tf.Session(graph=graph1) as sess:
-    sess.run(tf.global_variables_initializer())
-    saver.restore(sess, 'net/my_net.ckpt')
-    y_pred = np.zeros((y_test.shape[0],))
-    y_true = np.zeros((y_test.shape[0],))
+    with graph1.as_default():
+        x = tf.placeholder(tf.float64, [None, time_step * input_size])
+        # -1 指根据 x 来确定第一维的数据
+        image = tf.reshape(x, [-1, time_step, input_size])
+        y = tf.placeholder(tf.float64, [None, N_classes])
+        # 建立神经网络（3*100*2）
+        rnn_cell = tf.nn.rnn_cell.LSTMCell(num_units=num_units)
+        outputs, final_state = tf.nn.dynamic_rnn(cell=rnn_cell, inputs=image, dtype=tf.float64)
 
-    print(x_test[0])
-    print(x_test[0].reshape(1, -1))
-    for step in range(y_test.shape[0]):
-        prediction = sess.run(output, {x: x_test[step].reshape(1, -1)})
-        pred_one_hot = np.argmax(prediction, axis=1)
-        y_pred[step] = pred_one_hot[0]
-        # 450 * 1 -> 1 * 450
-        y_true[step] = np.argmax(y_test[step].reshape(1, -1))
-        print("第", step, "个数的预测结果为：", pred_one_hot[0], end=' ')
-        print("实际值为：", np.argmax(y_test[step].reshape(1, -1)))
+        # print(outputs) # ? * 150 * 100
+        # print(outputs[:, -1, :])
+        # 取最后一个时间戳的 outputs 并送入全连接层 得到 shape=(?, 2) 的 output
+        output = tf.layers.dense(inputs=outputs[:, -1, :], units=N_classes)
+        # print(output)
 
-print("Recall", recall_score(y_true, y_pred))
-print("Precision", precision_score(y_true, y_pred))
-print("CMatrix\n", confusion_matrix(y_true, y_pred))
+        # 用 softmax 求损失值
+        loss = tf.losses.softmax_cross_entropy(onehot_labels=y, logits=output)
+        # 用 Loss 反向传播，AdamOptimizer 梯度下降
+        train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+        # 取出概率最高的值 并判断 y 跟 output 是否是同一下标
+        # tf.argmax 返回一维向量中最大值的索引
+        # tf.equal 返回一个布尔型的列表
+        correct_prediction = tf.equal(tf.argmax(y, axis=1), tf.argmax(output, axis=1))
+        # print(correct_prediction) # shape=(?,1)
+
+        # tf.cast 把布尔型列表转换为 float32 ，如[T,T,F]，那么准确率为 66.6%
+        # tf.reduce_mean 对一维向量取平均值
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float'))
+
+        saver = tf.train.Saver()
+
+    with tf.Session(graph=graph1) as sess:
+        sess.run(tf.global_variables_initializer())
+        saver.restore(sess, 'net/my_net.ckpt')
+        y_pred = np.zeros((y_test.shape[0],))
+        y_true = np.zeros((y_test.shape[0],))
+
+        print(x_test[0])
+        print(x_test[0].reshape(1, -1))
+        for step in range(y_test.shape[0]):
+            prediction = sess.run(output, {x: x_test[step].reshape(1, -1)})
+            pred_one_hot = np.argmax(prediction, axis=1)
+            y_pred[step] = pred_one_hot[0]
+            # 450 * 1 -> 1 * 450
+            print(y_test[step])
+            y_true[step] = np.argmax(y_test[step].reshape(1, -1))
+
+            # print(pred_one_hot[0])
+            # print(np.argmax(y_test[step].reshape(1, -1)))
+
+            print("预测用户为：", pred_one_hot[0], end=' ')
+            print("实际用户为：", np.argmax(y_test[step].reshape(1, -1)))
+
+            if pred_one_hot[0] == np.argmax(y_test[step].reshape(1, -1)):
+                return '1'
+            else:
+                return '0'
+
+
+# print("Recall", recall_score(y_true, y_pred))
+# print("Precision", precision_score(y_true, y_pred))
+# print("CMatrix\n", confusion_matrix(y_true, y_pred))
+
+
+# train_handler(all_register, '7654321')
+# test_handler(test_src_path, '7654321')
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import os
 import data_process.data_handler_login
 import data_process.data_handler_register
 import alogirithm_model.svm.realize
+import alogirithm_model.lstm.realize
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -29,10 +30,9 @@ def register():
     # print('length:' + length + '\n')
     print('data:' + data + '\n')
     # 返回给客户端的 '1' 代表成功
-    flag = data_process.data_handler_register.handler(username, data)
-    if flag == True:
-        # 送进网络训练
-        return '1'
+    user_data_path = data_process.data_handler_register.handler(username, data)
+    # 送进网络训练
+    return alogirithm_model.lstm.realize.train_handler("G:\\handWritingRecognition\\Server\\data\\all_register.csv",username)
 
 
 # 此方法处理用户注册
@@ -50,11 +50,10 @@ def login():
     # 返回给客户端的 '1' 代表成功
     # 处理数据
     user_data_path = data_process.data_handler_login.handler(username, data)
-    return alogirithm_model.svm.realize.handler(user_data_path, username)
-    # # 送进网络预测
-    # # predict()
-    # run('')
-    # return '1'
+    # 送进网络预测
+    # return alogirithm_model.svm.realize.handler(user_data_path, username)
+    return alogirithm_model.lstm.realize.test_handler(user_data_path,username)
+
 
 
 @app.route('/')
