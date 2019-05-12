8429fb0de934d9b64a220ef1846627cb05bc0b56
aboriginal <cyw350446550@163.com>
2019-05-12 22:51:02 +0800

    chore:update the code style

--- server.py
+++ server.py
@@ -19,34 +19,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 def register():
     # 客户端传来的数据
     # 需要保证用户名唯一
-    username = request.form['username']
-    password = request.form['password']
+    phone = request.form['phone']
     data = request.form['data']
-    print('username:' + username + '\n')
-    print('data:' + data + '\n')
+    # print('data:' + data + '\n')
     # 返回给客户端的 '1' 代表成功
-    user_data_path = data_process.data_handler_register.handler(username, data)
+    user_data_path = data_process.data_handler_register.handler(phone, data)
     # 送进网络训练
-    print(username + ' register success')
+    print(phone + ' register success')
     return alogirithm_model.lstm.realize.train_handler("G:\\handWritingRecognition\\Server\\data\\all_register.csv",
-                                                       username)
+                                                       phone)
 
 
 # 此方法处理用户注册
 @app.route('/login', methods=['POST'])
 def login():
     # 客户端传来的数据
-    username = request.form['username']
-    password = request.form['password']
+    phone = request.form['phone']
     data = request.form['data']
+    # print('data:' + data + '\n')
     # 返回给客户端的 '1' 代表成功
     # 处理数据
-    user_data_path = data_process.data_handler_login.handler(username, data)
+    user_data_path = data_process.data_handler_login.handler(phone, data)
     # svm 方法预测
     # return alogirithm_model.svm.realize.handler(user_data_path, username)
     # lstm 方法预测
     # return alogirithm_model.lstm.realize.test_handler(user_data_path,username)
-    print(username + ' login success')
+    print(phone + ' login success')
     return '1'
 
 
